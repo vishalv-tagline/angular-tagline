b8c23131066e644ca7ec8ee8330d000217f7b6b5
Tagline <tagline@Taglines-iMac-Pro-64.local>
2022-12-24 18:12:16 +0530

    Implementing award section

--- src/app/award/award.component.ts
+++ src/app/award/award.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-award',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./award.component.scss']
 })
 export class AwardComponent implements OnInit {
+
+  @Input() awardDatas: string[] = [];
 
   constructor() { }
 
